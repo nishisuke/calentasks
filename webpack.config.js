8ba270fc2f9d6b8d93d55cdc0d599319ad3f0890
nishisuke <ryoskns+git@gmail.com>
2020-09-01 08:27:20 +0900

    f

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin') // optimization指定なしでデフォで入る。指定したら上書きされて消える
+
+const devMode = !!process.env.DEBUG
+
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+  },
+  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+            },
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  output: {
+    filename: devMode ? 'app.js' : 'app.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    // Reflect .envrc
+    new webpack.EnvironmentPlugin({
+      DEBUG: false,
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/static/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? 'style.css' : 'style.[hash].css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
+}

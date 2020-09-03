469d9d619d9bece1cf049e87cda50b3dc23319af
nishisuke <ryoskns+git@gmail.com>
2020-09-03 19:11:41 +0900

    scr

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,20 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC, ReactNode, useState, useRef } from 'react'
 
 interface Props {
   page: ReactNode
 }
 
 export const Layout: FC<Props> = ({ page }) => {
-  return <>{page}</>
+  const [scrollTop, setScrollTop] = useState(0)
+  const ref = useRef<HTMLDivElement>(null)
+
+  return (
+    <div
+      className="scroll-container"
+      onScroll={() => setScrollTop(ref.current?.scrollTop || 0)}
+      ref={ref}
+    >
+      {page}
+    </div>
+  )
 }

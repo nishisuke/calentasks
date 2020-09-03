95dff1ed01f28d995efd226d876df3e13215f2ba
nishisuke <ryoskns+git@gmail.com>
2020-09-03 20:07:00 +0900

    f

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useRef } from 'react'
+import React, { FC, useCallback, ReactNode, useEffect, useRef } from 'react'
 
 interface Props {
   page: ReactNode
@@ -12,14 +12,21 @@ declare global {
 
 export const Layout: FC<Props> = ({ page }) => {
   const ref = useRef<HTMLDivElement>(null)
+  const listener = useCallback(
+    () => (window.currentScrollTop = ref.current?.scrollTop || 0),
+    []
+  )
 
+  useEffect(() => {
+    ref.current?.addEventListener('scroll', listener, { passive: true })
+    return () => ref.current?.removeEventListener('scroll', listener, false)
+  }, [])
   return (
-    <div
-      className="scroll-container"
-      onScroll={() => (window.currentScrollTop = ref.current?.scrollTop || 0)}
-      ref={ref}
-    >
-      {page}
-    </div>
+    <>
+      <div className="scroll-container" ref={ref}>
+        {page}
+      </div>
+      <div className="foo"></div>
+    </>
   )
 }

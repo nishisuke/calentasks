3a15c69b5a8210da2e5a25f799d947ebec535095
nishisuke <ryoskns+git@gmail.com>
2020-09-09 21:23:20 +0900

    jst

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+}

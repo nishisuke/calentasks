9283044d91a36b504e17ae60bb55b17b32a57ffa
nishisuke <ryoskns+git@gmail.com>
2020-09-09 18:15:56 +0900

    f

--- src/components/ScrollOutSticky.tsx
+++ src/components/ScrollOutSticky.tsx
@@ -2,6 +2,6 @@ import React, { FC } from 'react'
 
 export const ScrollOutSticky: FC = ({ children }) => (
   <div data-scroll className="swipesticky">
-    <div className="scrollout hoge">{children}</div>
+    <div>{children}</div>
   </div>
 )

ecd96f7d8f9f3fc75f0746d719ea6782089105f0
nishisuke <ryoskns+git@gmail.com>
2020-09-18 19:58:33 +0900

    f

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 yarn run prd
+cp assets/* dist/
 yarn run -s firebase --project prd deploy
--- src/containers/SignIn.tsx
+++ src/containers/SignIn.tsx
@@ -6,6 +6,8 @@ interface M {}
 export const SignIn: FC<M> = ({}) => {
   return (
     <>
+      <p>sign in here</p>
+      <p>👇</p>
       <button onClick={signIn}>sign in</button>
     </>
   )

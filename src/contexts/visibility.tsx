e7c861fdf99739d7e1403d323b231cb65c65532a
nishisuke <ryoskns+git@gmail.com>
2020-09-18 20:36:28 +0900

    f

---
+++ src/contexts/visibility.tsx
@@ -0,0 +1,50 @@
+import React, { FC, createContext, useState, useEffect } from 'react'
+
+interface Props {
+  visible: boolean
+}
+
+export const VisibilityContext = createContext<Props>({
+  visible: true,
+})
+
+// @ts-ignore
+var hidden, visibilityChange
+// @ts-ignore
+if (typeof document.hidden !== 'undefined') {
+  // Opera 12.10 and Firefox 18 and later support
+  hidden = 'hidden'
+  visibilityChange = 'visibilitychange'
+  // @ts-ignore
+} else if (typeof document.msHidden !== 'undefined') {
+  hidden = 'msHidden'
+  visibilityChange = 'msvisibilitychange'
+  // @ts-ignore
+} else if (typeof document.webkitHidden !== 'undefined') {
+  hidden = 'webkitHidden'
+  visibilityChange = 'webkitvisibilitychange'
+}
+
+export const VisibilityProvider: FC = ({ children }) => {
+  const [value, set] = useState({ visible: true })
+  useEffect(() => {
+    const handleVisibilityChange = (e: any) => {
+      set({ visible: e.target.visibilityState === 'visible' })
+    }
+
+    // @ts-ignore
+    if (
+      !(
+        typeof document.addEventListener === 'undefined' || hidden === undefined
+      )
+    ) {
+      // @ts-ignore
+      document.addEventListener(visibilityChange, handleVisibilityChange, false)
+    }
+  }, [])
+  return (
+    <VisibilityContext.Provider value={value}>
+      {children}
+    </VisibilityContext.Provider>
+  )
+}
